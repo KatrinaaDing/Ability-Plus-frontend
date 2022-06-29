7980b49182c9652a4a554aaeff234a1b35f79fa6
dingheng <dinghengli666@gmail.com>
2022-06-30 01:16:19 +0800

    recover

--- src/examples/Footers/CenteredFooter/index.js
+++ src/examples/Footers/CenteredFooter/index.js
@@ -107,10 +107,28 @@ function CenteredFooter({ company, links, socials, light }) {
 CenteredFooter.defaultProps = {
   company: { href: "https://www.creative-tim.com/", name: "Creative Tim" },
   links: [
-
+    { href: "https://www.creative-tim.com/", name: "Company" },
+    { href: "https://www.creative-tim.com/presentation", name: "About Us" },
+    { href: "https://www.creative-tim.com/presentation", name: "Team" },
+    { href: "https://www.creative-tim.com/templates/react", name: "Products" },
+    { href: "https://www.creative-tim.com/blog", name: "Blog" },
+    { href: "https://www.creative-tim.com/license", name: "License" },
   ],
   socials: [
-    
+    { icon: <FacebookIcon fontSize="small" />, link: "https://www.facebook.com/CreativeTim/" },
+    {
+      icon: <TwitterIcon fontSize="small" />,
+      link: "https://twitter.com/creativetim",
+    },
+    {
+      icon: <InstagramIcon fontSize="small" />,
+      link: "https://www.instagram.com/creativetimofficial/",
+    },
+    {
+      icon: <PinterestIcon fontSize="small" />,
+      link: "https://ro.pinterest.com/thecreativetim/",
+    },
+    { icon: <GitHubIcon fontSize="small" />, link: "https://github.com/creativetimofficial" },
   ],
   light: false,
 };
