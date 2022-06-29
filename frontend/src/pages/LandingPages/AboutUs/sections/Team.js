58dc6cd8b447094705afae3b0dbd637a2c4beba7
dingheng <dinghengli666@gmail.com>
2022-06-29 16:56:58 +0800

    1

---
+++ frontend/src/pages/LandingPages/AboutUs/sections/Team.js
@@ -0,0 +1,51 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+
+// Material Kit 2 React examples
+import HorizontalTeamCard from "examples/Cards/TeamCards/HorizontalTeamCard";
+
+// Images
+import team1 from "assets/images/team-5.jpg";
+import team2 from "assets/images/bruce-mars.jpg";
+import team3 from "assets/images/ivana-squares.jpg";
+import team4 from "assets/images/ivana-square.jpg";
+
+function Team() {
+  return (
+    <MKBox
+      component="section"
+      variant="gradient"
+      bgColor="dark"
+      position="relative"
+      py={6}
+      px={{ xs: 2, lg: 0 }}
+      mx={-2}
+    >
+      <Container>
+        
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Team;
---
+++ frontend/src/pages/LandingPages/Author/index.js
@@ -0,0 +1,69 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Card from "@mui/material/Card";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+
+// Author page sections
+import Profile from "pages/LandingPages/Author/sections/Profile";
+import Posts from "pages/LandingPages/Author/sections/Posts";
+import Contact from "pages/LandingPages/Author/sections/Contact";
+import Footer from "pages/LandingPages/Author/sections/Footer";
+
+// Routes
+import routes from "routes";
+
+// Images
+import bgImage from "assets/images/city-profile.jpg";
+
+function Author() {
+  return (
+    <>
+      <DefaultNavbar
+        routes={routes}
+        transparent
+        light
+      />
+      <MKBox bgColor="white">
+        <MKBox
+          minHeight="25rem"
+          width="100%"
+          sx={{
+            backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+              `${linearGradient(
+                rgba(gradients.dark.main, 0.8),
+                rgba(gradients.dark.state, 0.8)
+              )}, url(${bgImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            display: "grid",
+            placeItems: "center",
+          }}
+        />
+
+        <Contact />
+        <Footer />
+      </MKBox>
+    </>
+  );
+}
+
+export default Author;
