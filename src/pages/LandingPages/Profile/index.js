292e46b5787438cb0d90d7e00da9bbd939ae930e
dingheng <dinghengli666@gmail.com>
2022-07-02 20:42:37 +0800

    Modal of 'Edit Profile'

--- src/pages/LandingPages/Profile/index.js
+++ src/pages/LandingPages/Profile/index.js
@@ -5,6 +5,11 @@ import Card from "@mui/material/Card";
 import Switch from "@mui/material/Switch";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
+import Modal from "@mui/material/Modal";
+import Slide from "@mui/material/Slide";
+import Divider from "@mui/material/Divider";
+
+import CloseIcon from "@mui/icons-material/Close";
 
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
@@ -20,6 +25,9 @@ import DefaultFooter from "examples/Footers/DefaultFooter";
 import footerRoutes from "footer.routes";
 
 const ProfileBasic = () => {
+
+    const [show, setShow] = useState(false);
+    const toggleModal = () => setShow(!show);
 
     return (
         <>
@@ -99,10 +107,52 @@ const ProfileBasic = () => {
                         <MKButton variant="outlined" color="error" size="small">
                             Delete Account
                         </MKButton>
-                        <MKButton variant="outlined" color="info" size="small">
+                        <MKButton variant="outlined" color="info" size="small" onClick={toggleModal}>
                             Edit Profile
                         </MKButton>
-                </Grid>                     
+                </Grid>
+                <Modal open={show} onClose={toggleModal} sx={{display:"grid", placeItems:"center"}}>
+                    <Slide direction="down" in={show} timeout={500}>
+                        <MKBox
+                            position="relative"
+                            width="30%"
+                            display="flex"
+                            flexDirection="column"
+                            borderRadius="xl"
+                            bgColor="white"
+                            shadow="xl"    
+                        >
+                            <MKBox display="flex" alginItems="center" justifyContent="space-between" p={3}>
+                                <MKTypography variant="h5">Profile Edit</MKTypography>
+                                <CloseIcon fontSize="medium" sx={{ cursor:"pointer" }} onClick={toggleModal} />
+                            </MKBox>
+                            <Divider sx={{my: 0}} />
+                            <MKBox component="form" role="form" p={2} py={12}>
+{/* The 'value' of Input should be parameters which have state change. */}                               
+                                <Grid container item xs={12}>
+                                    <MKInput label="Username" fullWidth value="Jane Wong" />
+                                </Grid>     
+                                <br />
+                                <Grid container item xs={12}>
+                                    <MKInput type="email" label="Email" fullWidth value="JaneWong@sample.com" />
+                                </Grid> 
+                                <br />
+                                <Grid container item xs={12}>
+                                    <MKInput label="Description" multiline fullWidth value="xxxxxxxxxxx sample here" rows={6} />
+                                </Grid>                                                           
+                            </MKBox>
+                            <Divider sx={{my: 0}} />
+                            <MKBox display="flex" justifyContent="space-between" p={1.5}>
+                                <MKButton variant="gradient" color="dark" onClick={toggleModal}>
+                                Close
+                                </MKButton>
+                                <MKButton variant="gradient" color="info">
+                                Save
+                                </MKButton>
+                            </MKBox>                            
+                        </MKBox>
+                    </Slide>
+                </Modal>                     
             </Container>
         </MKBox>
 
