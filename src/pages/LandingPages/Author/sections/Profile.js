7162a1ae2c3b1e6405034f81b6115d87a244563f
dingheng <dinghengli666@gmail.com>
2022-07-17 11:32:02 +0800

    profile & companyinfo
    profile complete.
    companyinfo ui change

--- src/pages/LandingPages/Author/sections/Profile.js
+++ src/pages/LandingPages/Author/sections/Profile.js
@@ -15,7 +15,7 @@ Coded by www.creative-tim.com
 
 // @mui material components
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
@@ -41,79 +41,147 @@ import profilePicture from "assets/images/bruce-mars.jpg";
 import { Axios } from "axios";
 import useAxiosBasic from "hooks/useAxiosBasic";
 import { axiosPrivate } from "api/axios";
+import useAxiosPrivate from "hooks/useAxiosPrivate";
 
 
 function Profile() {
 
-  const axiosBasic = useAxiosBasic();
-  const {auth, setAuth} = useAuth();
-  const user = auth.username;
+  const axiosPrivate = useAxiosPrivate();
+  const { auth, setAuth } = useAuth();
 
   const [show, setShow] = useState(false);
-  const toggleModal = () => setShow(!show);
+  const toggleModal = () =>{
+    setShow(!show);
+  } 
 
   const [userName, setUserName] = useState("");
   const [des, setDes] = useState("");
   const [age, setAge] = useState("");
-  const [oldPwd, setOldPwd] = useState("");
-  const [newPwd, setNewPwd] = useState("");
-  const [newPwdConfirm, setNewPwdConfirm] = useState("");
   const [userEmail, setUserEmail] = useState("");
   const [alertStr, setAlertStr] = useState("");
-  const [alertModalOpen, setAlertModalOpen] = useState(false);
 
-  const navigate = useNavigate();
+  const [fullName, setFullName] = useState("");
+  const [cEmail, setCEmail] = useState(localStorage.getItem("cEmail"));
+  const [cAge, setCAge] = useState(localStorage.getItem("cAge"));
+  const [cDes, setCDes] = useState(localStorage.getItem("cDes"));
+
+  useEffect (async () => {
+    await axiosPrivate.get("/user/get_profile_info")
+      .then (res => {
+        if(res.data.fullName){
+          setAuth({
+            username:  res.data.fullName,
+            isCompany: auth.isCompany,
+            accessToken: auth.accessToken
+          })
+        }
+        if (JSON.parse(res.data.extraData).email === null) {
+          setCEmail("sample@email.com")
+        } else {
+          setCEmail(JSON.parse(res.data.extraData).email)
+        }
+        if (JSON.parse(res.data.extraData).des === null) {
+          setCDes("Please introduce yourself")
+        } else {
+          setCDes(JSON.parse(res.data.extraData).des)
+        }
+        if (JSON.parse(res.data.extraData).age === null) {
+          setCAge("?")
+        } else {
+          setCAge(JSON.parse(res.data.extraData).age)
+        }
+
+      })
+      .catch(e => console.error(e))
+  }, [])
 
   const changeProfile = (event) => {
-
-    const body = {
-      extraData: {
-        des: des.des,
-        age: age.age,
-        email: userEmail.userEmail,
-      },
-      newPassword: newPwd.newPwd,
-      oldPassword: oldPwd.oldPwd,
-      userName: userName.userName
+    if(des.des === ''){
+      return setAlertStr("Description is empty!")
     }
-    console.log(body)
+    if(age.age === ''){
+      return setAlertStr("Age is empty!")
+    }
+    if(userEmail.userEmail === ''){
+      return setAlertStr("Contact email is necessary!")
+    }
+    if(userName.userName === ''){
+      return setAlertStr("Username is empty")
+    }
+    const body = {
+      "extraData": {
+        "des": des.des || cDes,
+        "age": age.age || cAge,
+        "email": userEmail.userEmail || cEmail,
+      },
+      "userName": userName.userName || auth.username
+    }
+/*     console.log(body)
     console.log(JSON.stringify(body))
-
+    console.log(userName.userName)
+    console.log(auth.username)
+    console.log(cAge)
+    console.log(cDes)
+    console.log(cEmail) */
     if (Object.is(event.target.name, "save")) {
       axiosPrivate.post("/user/edit_own_profile_info", body)
-        .then(function (response) {
-          console.log("response:", response);
-          if (response.code == 201) {
-            return setAlertStr(response.data.message);
+        .then(function (res) {
+          console.log("response:", res);
+          if (res.code == 201) {
+            return setAlertStr(res.data.message)
           }
+          localStorage.setItem("cDes", body.extraData.des)
+          localStorage.setItem("cAge", body.extraData.des)
+          localStorage.setItem("cEmail", body.extraData.email)
+          localStorage.setItem("userName",  userName.userName)
+
+          setAuth({
+            username:  userName.userName,
+            isCompany: auth.isCompany,
+            accessToken: auth.accessToken
+          })
+          
         })
         .catch (err => {
-          setAlertStr(response.data.message)
+          if (err !== undefined && err.response !== undefined && err.response.data !== undefined) {
+            setAlertStr(err.res.data.error);
+          }
+          else if (err !== undefined) {
+            setAlertStr(err.message)
+          } else {
+            setAlertStr("Failed to change profile")
+          }
         })
     }
     setTimeout(function () {
       setAlertStr("");
-    }, 3000);  
+    }, 3000);
+
+    window.location.reload();
   }
 
-  const updateUserName = e => {
+  const updateUserName = e => { 
     const userName = e.target.value
     setUserName({userName})
+    localStorage.setItem("userName", userName)
   }
 
   const updateUserEmail = e => {
     const userEmail = e.target.value
     setUserEmail ({userEmail})
+    localStorage.setItem("cEmail", userEmail)
   }
 
   const updateUserDes = e => {
     const des = e.target.value
-    setDes({des})
+    setDes({des});
+    localStorage.setItem("cDes", des)
   }
 
   const updateUserAge = e => {
     const age = e.target.value
-    setAge({age})
+    setAge({age});
+    localStorage.setItem("cAge", age)
   } 
 
   return (
@@ -128,7 +196,7 @@ function Profile() {
             <Grid item xs={12} md={7} mx={{ xs: "auto", sm: 6, md: 1 }}>
               <MKBox display="flex" justifyContent="space-between" alignItems="center" mb={1}>
                 <MKTypography variant="h3">
-                  {userName.userName}
+                  {auth.username}
                 </MKTypography>
                 <MKButton variant="outlined" color="info" size="small" onClick={toggleModal}>
                   Edit Profile
@@ -143,13 +211,13 @@ function Profile() {
                     Contact Email&nbsp;&nbsp;
                   </MKTypography>
                   <MKTypography component="span" variant="body2" color="text">
-                    {userEmail.userEmail}&nbsp;&nbsp;&nbsp;
+                    {cEmail}&nbsp;&nbsp;&nbsp;
                   </MKTypography>
                   <MKTypography component="span" variant="body2" fontWeight="bold">
                     Age&nbsp;&nbsp;
                   </MKTypography>
                   <MKTypography component="span" variant="body2" color="text">
-                    {age.age}
+                    {cAge}
                   </MKTypography>
                 </Grid>
               </Grid>
@@ -157,7 +225,7 @@ function Profile() {
                 Description&nbsp;&nbsp;
               </MKTypography>
               <MKTypography variant="body1" component="span" fontWeight="light" color="text">
-                {des.des}
+                {cDes}
               </MKTypography>
             </Grid>
           </Grid>
@@ -185,19 +253,19 @@ function Profile() {
                     <MKBox component="form" role="form" p={2} py={12}>
 {/* The 'value' of Input should be parameters which have state change. */}                               
                         <Grid container item xs={12}>
-                            <MKInput label="Username" fullWidth defaultValue={userName.userName} onChange={updateUserName} />
-                        </Grid>     
+                            <MKInput label="Username" fullWidth defaultValue={auth.username} onChange={updateUserName} />
+                        </Grid>      
                         <br />
                         <Grid container item xs={12}>
-                            <MKInput type="email" label="Contact Email" onChange={updateUserEmail} defaultValue={userEmail.userEmail} fullWidth />
+                            <MKInput type="email" label="Contact Email" onChange={updateUserEmail} defaultValue={cEmail} fullWidth />
                         </Grid> 
                         <br />
                         <Grid container item xs={12}>
-                            <MKInput label="Age" fullWidth defaultValue={age.age} onChange={updateUserAge} />
+                            <MKInput label="Age" fullWidth defaultValue={cAge} onChange={updateUserAge} />
                         </Grid>    
                         <br />
                         <Grid container item xs={12}>
-                            <MKInput label="Description" multiline fullWidth onChange={updateUserDes} defaultValue={des.des} rows={6} />
+                            <MKInput label="Description" multiline fullWidth onChange={updateUserDes} defaultValue={cDes} rows={6} />
                         </Grid>                                                           
                     </MKBox>
                     <Divider sx={{my: 0}} />
