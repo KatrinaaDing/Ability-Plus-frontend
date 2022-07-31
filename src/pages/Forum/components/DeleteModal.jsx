bc8cc06e540641c1b1021d5d26bf9d9d8cdc4634
KatrinaaDing <xiaoding1998@hotmail.com>
2022-07-31 21:43:57 +1000

    edit and delete post

---
+++ src/pages/Forum/components/DeleteModal.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import { Divider, Grid, IconButton, Modal, Slide, Switch } from '@mui/material';
+import MKBox from 'components/MKBox';
+import MKTypography from 'components/MKTypography';
+import CloseIcon from "@mui/icons-material/Close";
+import MKButton from 'components/MKButton';
+import useAuth from 'auth/useAuth';
+import useAxiosPrivate from 'hooks/useAxiosPrivate';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+const DeleteModal = ({postId}) => {
+    // hooks
+    const { auth } = useAuth();
+    const axiosPrivate = useAxiosPrivate();
+
+    // states
+    const [open, setOpen] = React.useState(false)
+    const [pin, setPin] = React.useState(false)
+
+    // handlers
+    const handleClose = () => setOpen(false)
+
+    const handleDelete = (e) => {
+        const params = new URLSearchParams();
+        params.append('postId', postId)
+
+        // append query string after url if using POST
+        axiosPrivate.post('/forum/post/delete_my_post?' + params.toString())
+            .then(res => {
+                setOpen(false)
+                location.reload()
+            })
+            .catch(e => console.error(e))
+    }
+
+    return (
+        <>
+            <IconButton
+                edge="end"
+                aria-label="delete"
+                sx={{ mr: 0.5, opacity: 0.7, fontSize: 'lg' }}
+                color='error'
+                onClick={() => setOpen(true)}
+                alt='Delete the post'
+            >
+                <DeleteIcon />
+            </IconButton>
+            <Modal open={open} onClose={handleClose} sx={{ display: "grid", placeItems: "center" }}>
+                <Slide direction="down" in={open} timeout={500}>
+                    <MKBox
+                        position="relative"
+                        width="20%"
+                        display="flex"
+                        flexDirection="column"
+                        borderRadius="xl"
+                        bgColor="white"
+                        shadow="xl"
+                    >
+                        <MKBox display="flex" alginItems="center" justifyContent="space-between" p={3}>
+                            <MKTypography variant="h6">Delete Confirm</MKTypography>
+                            <CloseIcon fontSize="medium" sx={{ cursor: "pointer" }} onClick={handleClose} />
+                        </MKBox>
+                        <MKTypography variant="body2" color="secondary" fontWeight="regular" textAlign="center" sx={{ py: 2 }}>
+                            All replies will also be deleted. <br/>
+                            You sure want to delete?
+                        </MKTypography>
+                        <MKBox display="flex" justifyContent="space-between" p={1.5}>
+                            <MKButton variant="gradient" color="light" onClick={handleClose}>
+                                No
+                            </MKButton>
+                            <MKButton variant="gradient" color="info" name="confirm" onClick={handleDelete}>
+                                Yes
+                            </MKButton>
+                        </MKBox>
+                    </MKBox>
+                </Slide>
+            </Modal>  
+        
+        </>
+    );
+};
+
+export default DeleteModal;
--- src/pages/Forum/components/Post.jsx
+++ src/pages/Forum/components/Post.jsx
@@ -14,14 +14,17 @@ import MKBox from 'components/MKBox';
 import Modal from "@mui/material/Modal";
 import Slide from "@mui/material/Slide";
 import CloseIcon from "@mui/icons-material/Close";
+import DeleteIcon from '@mui/icons-material/Delete';
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import MKButton from "components/MKButton";
 import Reply from "../../MyPosts/components/Reply"
 import MKInput from 'components/MKInput';
 import { useState } from 'react';
+import EditModal from './EditModal';
+import DeleteModal from './DeleteModal';
 
-const Post = ({ id, isProjectOwner, authorId, authorName, content, postDate, numReply, isPin}) => {
+const Post = ({ id, isProjectOwner, authorId, authorName, content, postDate, numReply, isPin }) => {
     const { auth } = useAuth();
     const [show, setShow] = useState(false)
     const toggleModal = () => {
@@ -33,26 +36,41 @@ const Post = ({ id, isProjectOwner, authorId, authorName, content, postDate, num
         setRShow(!rShow);
     }
 
+   
+
     return (
         <ListItem
             alignItems="flex-start"
             secondaryAction={
-                <IconButton 
-                    edge="end" 
-                    aria-label="comments" 
-                    sx={{mr: 2, opacity: 0.5, fontSize: 'lg'}}
-                    onClick={toggleModal}
-                >
-                    <CommentIcon /> &nbsp;
-                    <MKTypography variant='subtitle2'>{numReply}</MKTypography>
-                </IconButton>
+                <>
+                    {
+                        // display edit and delete button for user's posts
+                        authorName === auth.username &&
+                        <>
+                            <DeleteModal postId={id} />
+                            <EditModal postId={id} content={content}/>
+                        </>
+                    }
+
+                    <IconButton
+                        edge="end"
+                        aria-label="comments"
+                        sx={{ mr: 2, opacity: 0.5, fontSize: 'lg' }}
+                        onClick={toggleModal}
+                    >
+                        <CommentIcon /> &nbsp;
+                        <MKTypography variant='subtitle2'>{numReply}</MKTypography>
+                    </IconButton>
+
+
+                </>
             }
             sx={{
                 p: 2,
                 borderBottom: 'solid 0.3px gray'
             }}
         >
-            
+
             <ListItemAvatar>
                 <Avatar alt={authorName} src="/src/assets/images/profile-avatars/company.png" />
             </ListItemAvatar>
@@ -66,63 +84,63 @@ const Post = ({ id, isProjectOwner, authorId, authorName, content, postDate, num
                                     ? authorName + ' (Me)'
                                     : authorName
                         }
-                        <PushPinIcon color='warning' fontSize='medium' opacity={Number(isPin)} sx={{mt: 'auto', mb: 'auto', ml: 1}}/>
+                        <PushPinIcon color='warning' fontSize='medium' opacity={Number(isPin)} sx={{ mt: 'auto', mb: 'auto', ml: 1 }} />
                     </MKBox>
                 }
                 secondary={
                     <>
-                        <MKTypography variant='caption'>{new Date(postDate * 1000).toLocaleString()}</MKTypography><br/>
-                        <MKTypography variant='body'>{content}</MKTypography> 
+                        <MKTypography variant='caption'>{new Date(postDate * 1000).toLocaleString()}</MKTypography><br />
+                        <MKTypography variant='body'>{content}</MKTypography>
                     </>
                 }
                 sx={{
-                    mr: 10,
+                    mr: 20,
                 }}
             />
             <Modal open={show} onClose={toggleModal} sx={{ display: "grid", placeItems: "center" }}>
                 <Slide direction="down" in={show} timeout={500}>
                     <MKBox
-                    position="relative"
-                    width="60%"
-                    display="flex"
-                    flexDirection="column"
-                    borderRadius="xl"
-                    bgColor="white"
-                    shadow="xl"
-                    >      
+                        position="relative"
+                        width="60%"
+                        display="flex"
+                        flexDirection="column"
+                        borderRadius="xl"
+                        bgColor="white"
+                        shadow="xl"
+                    >
                         <MKBox display="flex" alginItems="center" justifyContent="space-between" p={3}>
                             <MKTypography variant="h5">Post Detail</MKTypography>
                             <CloseIcon fontSize="medium" sx={{ cursor: "pointer" }} onClick={toggleModal} />
-                        </MKBox>         
-{/*                         <Divider sx={{ my: 0 }} />    */}  
+                        </MKBox>
+                        {/*                         <Divider sx={{ my: 0 }} />    */}
                         <MKBox component="section">
                             <Container>
                                 <Grid container spacing={2} mb={3} justifyContent="space-between">
                                     <MKTypography component="div" variant="body1" fontWeight="bold" >
-                                    Main Post
+                                        Main Post
                                     </MKTypography>
                                     <MKButton variant="gradient" color="light" size="small">
-                                    Go To Forum
+                                        Go To Forum
                                     </MKButton>
                                     <Reply />
                                 </Grid>
                                 <br />
-{/*                                 <Divider sx={{ my: 0 }} />    */}
+                                {/*                                 <Divider sx={{ my: 0 }} />    */}
                                 <Grid container spacing={2} mb={3} justifyContent="space-between">
                                     <MKTypography component="div" variant="body1" fontWeight="bold" >
-                                    Replies
+                                        Replies
                                     </MKTypography>
                                     <MKButton variant="gradient" color="info" size="small" onClick={replyModal}>
-                                    Create Reply
+                                        Create Reply
                                     </MKButton>
                                     <Reply />
                                 </Grid>
                             </Container>
                         </MKBox>
                     </MKBox>
-                </Slide>          
-            </Modal>      
-{/* Create Reply Modal */}
+                </Slide>
+            </Modal>
+            {/* Create Reply Modal */}
             <Modal open={rShow} onClose={replyModal} sx={{ display: "grid", placeItems: "center" }}>
 
                 <Slide direction="down" in={rShow} timeout={500}>
@@ -138,16 +156,16 @@ const Post = ({ id, isProjectOwner, authorId, authorName, content, postDate, num
                         <MKBox display="flex" alginItems="center" justifyContent="space-between" p={3}>
                             <MKTypography variant="h5">Create Reply</MKTypography>
                             <CloseIcon fontSize="medium" sx={{ cursor: "pointer" }} onClick={replyModal} />
-                            </MKBox>
-                            <Divider sx={{ my: 0 }} />
-                            <MKBox component="form" role="form" p={2} py={12}>
+                        </MKBox>
+                        <Divider sx={{ my: 0 }} />
+                        <MKBox component="form" role="form" p={2} py={12}>
                             <Grid container item xs={12}>
                                 <MKInput type="text" label="Reply" multiline fullWidth rows={6} defaultValue="Say Something..." />
                             </Grid>
 
-                            </MKBox>
-                            <Divider sx={{ my: 0 }} />
-                            <MKBox display="flex" justifyContent="space-between" p={1.5}>
+                        </MKBox>
+                        <Divider sx={{ my: 0 }} />
+                        <MKBox display="flex" justifyContent="space-between" p={1.5}>
                             <MKButton variant="gradient" color="light" onClick={replyModal}>
                                 Cancel
                             </MKButton>
@@ -157,7 +175,7 @@ const Post = ({ id, isProjectOwner, authorId, authorName, content, postDate, num
                         </MKBox>
                     </MKBox>
                 </Slide>
-            </Modal>     
+            </Modal>
         </ListItem>
     );
 };
